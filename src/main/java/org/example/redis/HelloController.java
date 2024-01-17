dc026ca95888047fd9a304517faf59b07a44083f
JeongJinUng <jinung.dev@gmail.com>
2024-01-18 02:31:44 +0900

    :sparkles: Redis 연동 개발

--- src/main/java/org/example/redis/HelloController.java
+++ src/main/java/org/example/redis/HelloController.java
@@ -1,14 +1,38 @@
 package org.example.redis;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    private final StringRedisTemplate stringRedisTemplate;
+
+    public HelloController(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
     @GetMapping("/hello")
     public String hello() {
         return "hello world!";
+    }
+
+    @GetMapping("/setFruit")
+    public String setFruit(@RequestParam String name) {
+        ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+        ops.set("fruit", name);
+        return "saved";
+    }
+
+    @GetMapping("/getFruit")
+    public String getFruit() {
+        ValueOperations<String, String> ops = stringRedisTemplate.opsForValue();
+        String fruitName = ops.get("fruit");
+        return fruitName;
     }
 
 
